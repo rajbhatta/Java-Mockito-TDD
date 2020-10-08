515db07fbbac95cb9cea9143c0ad0a38755e1d95
Raj Bhatta <bhattaraj21@gmail.com>
2020-10-08 00:20:23 -0700

    Details about mockito stubbing

--- 03-method-stubbing/src/test/java/raj/mockito/revise/AccountServiceTest.java
+++ 03-method-stubbing/src/test/java/raj/mockito/revise/AccountServiceTest.java
@@ -1,7 +1,6 @@
 package raj.mockito.revise;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -16,25 +15,36 @@ class AccountServiceTest {
     //Create instance of AccountService
     private AccountService underTest;
 
-    private BackgroundCheckService backgroundCheckService=mock(BackgroundCheckService.class);
-    private ReferenceIdsManager referenceIdsManager=mock(ReferenceIdsManager.class);
-    private AccountRepository accountRepository=mock(AccountRepository.class);
+    private BackgroundCheckService backgroundCheckService = mock(BackgroundCheckService.class);
+    private ReferenceIdsManager referenceIdsManager = mock(ReferenceIdsManager.class);
+    private AccountRepository accountRepository = mock(AccountRepository.class);
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         //We are injecting mock this time
-        underTest=new AccountService(backgroundCheckService,referenceIdsManager,accountRepository);
+        underTest = new AccountService(backgroundCheckService, referenceIdsManager, accountRepository);
+    }
+
+    @Test
+    public void shouldOpenAccount() throws IOException {
+
+        //This return will direct us to the else condition
+        when(backgroundCheckService.confirm("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2))).thenReturn(new BackgroundCheckResults("something_not_unacceptable", 100));
+        when(referenceIdsManager.obtainId("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2))).thenReturn("some_id");
+
+        final AccountOpeningStatus accountOpeningStatus = underTest.openAccount("Raj", "Bhatta", "123", LocalDate.of(2020, 10, 10));
+        assertEquals(AccountOpeningStatus.OPENED, accountOpeningStatus);
     }
 
     @Test
     public void shouldDeclineAccount() throws IOException {
 
-       final String UNACCEPTABLE_RISK_PROFILE = "HIGH";
+        final String UNACCEPTABLE_RISK_PROFILE = "HIGH";
 
         //Let's add method subbing
-        when(backgroundCheckService.confirm("Raj","Bhatta","1234", LocalDate.of(2020,10,2))).thenReturn(new BackgroundCheckResults(UNACCEPTABLE_RISK_PROFILE,0));
+        when(backgroundCheckService.confirm("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2))).thenReturn(new BackgroundCheckResults(UNACCEPTABLE_RISK_PROFILE, 0));
 
-        final AccountOpeningStatus accountOpeningStatus=underTest.openAccount("Raj","Bhatta","1234", LocalDate.of(2020,10,2));
-        assertEquals(AccountOpeningStatus.DECLINED,accountOpeningStatus);
+        final AccountOpeningStatus accountOpeningStatus = underTest.openAccount("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2));
+        assertEquals(AccountOpeningStatus.DECLINED, accountOpeningStatus);
     }
 }
