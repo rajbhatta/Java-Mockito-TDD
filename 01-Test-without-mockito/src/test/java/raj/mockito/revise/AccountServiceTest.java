6d167a1ceaff1eaa5c37a6a7d7e0a90b8e248929
Raj Bhatta <bhattaraj21@gmail.com>
2020-10-07 23:08:18 -0700

    Junit test without mockito

--- 01-Test-without-mockito/src/test/java/raj/mockito/revise/AccountServiceTest.java
+++ 01-Test-without-mockito/src/test/java/raj/mockito/revise/AccountServiceTest.java
@@ -1,10 +1,27 @@
 package raj.mockito.revise;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AccountServiceTest {
 
+    //Create instance of AccountService
+    private AccountService underTest;
+
+    @BeforeEach
+    public void setup(){
+        //We are injecting null without doing mock
+        underTest=new AccountService(null,null,null);
+    }
+
     @Test
-    void openAccount() {
+    void openAccountTest() throws IOException {
+        final AccountOpeningStatus accountOpeningStatus=underTest.openAccount("Raj","Bhatta","1234", LocalDate.of(2020,10,2));
+        assertEquals(AccountOpeningStatus.OPENED,accountOpeningStatus);
     }
 }
