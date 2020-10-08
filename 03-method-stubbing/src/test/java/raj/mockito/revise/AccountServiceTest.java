56824438dde79b661b8bc07bfc2bc2381b48a4fb
Raj Bhatta <bhattaraj21@gmail.com>
2020-10-08 00:27:25 -0700

    Method stubbing example in Mockito

--- 03-method-stubbing/src/test/java/raj/mockito/revise/AccountServiceTest.java
+++ 03-method-stubbing/src/test/java/raj/mockito/revise/AccountServiceTest.java
@@ -7,44 +7,55 @@ import java.io.IOException;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class AccountServiceTest {
 
-    //Create instance of AccountService
-    private AccountService underTest;
+  // Create instance of AccountService
+  private AccountService underTest;
 
-    private BackgroundCheckService backgroundCheckService = mock(BackgroundCheckService.class);
-    private ReferenceIdsManager referenceIdsManager = mock(ReferenceIdsManager.class);
-    private AccountRepository accountRepository = mock(AccountRepository.class);
+  private BackgroundCheckService backgroundCheckService = mock(BackgroundCheckService.class);
+  private ReferenceIdsManager referenceIdsManager = mock(ReferenceIdsManager.class);
+  private AccountRepository accountRepository = mock(AccountRepository.class);
 
-    @BeforeEach
-    public void setup() {
-        //We are injecting mock this time
-        underTest = new AccountService(backgroundCheckService, referenceIdsManager, accountRepository);
-    }
+  String NAME = "Raj";
+  String LAST_NAME = "Bhatta";
+  String TAX_ID = "1234";
+  LocalDate OPENDED_DATE = LocalDate.of(2020, 10, 2);
 
-    @Test
-    public void shouldOpenAccount() throws IOException {
+  @BeforeEach
+  public void setup() {
+    // We are injecting mock this time
+    underTest = new AccountService(backgroundCheckService, referenceIdsManager, accountRepository);
+  }
 
-        //This return will direct us to the else condition
-        when(backgroundCheckService.confirm("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2))).thenReturn(new BackgroundCheckResults("something_not_unacceptable", 100));
-        when(referenceIdsManager.obtainId("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2))).thenReturn("some_id");
+  @Test
+  public void shouldOpenAccount() throws IOException {
 
-        final AccountOpeningStatus accountOpeningStatus = underTest.openAccount("Raj", "Bhatta", "123", LocalDate.of(2020, 10, 10));
-        assertEquals(AccountOpeningStatus.OPENED, accountOpeningStatus);
-    }
+    // This return will direct us to the else condition
+    when(backgroundCheckService.confirm(NAME, LAST_NAME, TAX_ID, OPENDED_DATE))
+        .thenReturn(new BackgroundCheckResults(anyString(), 100));
 
-    @Test
-    public void shouldDeclineAccount() throws IOException {
+    when(referenceIdsManager.obtainId(NAME, LAST_NAME, TAX_ID, OPENDED_DATE)).thenReturn("some_id");
 
-        final String UNACCEPTABLE_RISK_PROFILE = "HIGH";
+    final AccountOpeningStatus accountOpeningStatus =
+        underTest.openAccount("Raj", "Bhatta", "123", LocalDate.of(2020, 10, 10));
+    assertEquals(AccountOpeningStatus.OPENED, accountOpeningStatus);
+  }
 
-        //Let's add method subbing
-        when(backgroundCheckService.confirm("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2))).thenReturn(new BackgroundCheckResults(UNACCEPTABLE_RISK_PROFILE, 0));
+  @Test
+  public void shouldDeclineAccount() throws IOException {
 
-        final AccountOpeningStatus accountOpeningStatus = underTest.openAccount("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2));
-        assertEquals(AccountOpeningStatus.DECLINED, accountOpeningStatus);
-    }
+    final String UNACCEPTABLE_RISK_PROFILE = "HIGH";
+
+    // Let's add method subbing
+    when(backgroundCheckService.confirm("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2)))
+        .thenReturn(new BackgroundCheckResults(UNACCEPTABLE_RISK_PROFILE, 0));
+
+    final AccountOpeningStatus accountOpeningStatus =
+        underTest.openAccount("Raj", "Bhatta", "1234", LocalDate.of(2020, 10, 2));
+    assertEquals(AccountOpeningStatus.DECLINED, accountOpeningStatus);
+  }
 }
